58830e53b09fde1c53dcb44a574995dca2193e9b
moritzdanhausen <morit.danhausen@gmx.de>
2022-01-31 09:21:24 +0100

    add changes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ViewbooksComponent } from './viewbooks/viewbooks.component';
+const routes: Routes = [ { path:'', component:ViewbooksComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
